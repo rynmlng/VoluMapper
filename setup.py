e0118223358962e673526bd954b98c0e29aa0ce0
Ryan Miling <ryanmiling@gmail.com>
2016-05-23 21:40:05 -0400

    Changing package name

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ VERSION = "1.0.0"
 setup(name="VoluMapper",
       version=VERSION,
       descripton="Maps EBS Volumes to EC2 Instances in a table",
-      packages=["pollers"],
+      packages=["volumapper"],
       url="https://github.com/rynmlng/VolumMapper",
       author="Ryan Miling",
       author_email="ryanmiling@gmail.com",
       license="MIT",
       entry_points={
-          "console_scripts": ["volumapper=pollers.aws_poller:main",
-                              "volumapper_utils=pollers.utils:main",
+          "console_scripts": ["volumapper=volumapper.aws_poller:main",
+                              "volumapper_utils=volmapper.utils:main",
                              ]
       }
 )
